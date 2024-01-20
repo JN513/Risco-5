b19c8c212793d9774874b481e6c364ba1201ac53
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-01-20 19:50:12 -0300

    criando test de core

--- tests/core_test.v
+++ tests/core_test.v
@@ -1,14 +1,31 @@
 module core_tb();
 
 reg clk, reset;
+wire memory_read, memory_write;
+wire [31:0] address, write_data, read_data;
 
 always #1 clk = ~clk;
 
-Core #(
-    .MEMORY_FILE("software/memory/bne.hex")
-) Core(
+Core Core(
     .clk(clk),
-    .reset(reset)
+    .reset(reset),
+    .memory_read(memory_read),
+    .memory_write(memory_write),
+    .write_data(write_data),
+    .read_data(read_data),
+    .address(address)
+);
+
+Memory #(
+    .MEMORY_FILE("software/memory/addi.hex")
+) Memory(
+    .clk(clk),
+    .reset(reset),
+    .memory_read(memory_read),
+    .memory_write(memory_write),
+    .write_data(write_data),
+    .read_data(read_data),
+    .address(address)
 );
 
 initial begin
@@ -16,6 +33,11 @@ initial begin
     $dumpvars;
 
     clk = 0;
+
+    reset = 1;
+
+    #6
+
     reset = 0;
 
     #120
