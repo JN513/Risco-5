c41789bfb7c42b972e4af521be6bc7b6f94b4768
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-03-18 15:59:18 -0300

    Adicionando script para compilar todos os codigos em simultaneo

---
+++ software/compile_all.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+diretorio="code"
+march="rv32i"
+
+# Verifica se o diretório existe
+if [ -d "$diretorio" ]; then
+    # Loop pelos arquivos no diretório
+    for arquivo in "$diretorio"/*; do
+        # Verifica se é um arquivo regular
+        if [ -f "$arquivo" ]; then
+            echo "Compilando $arquivo"  # Faça o que desejar com o arquivo
+            nome_sem_extensao=$(basename "$arquivo" | sed 's/\.[^.]*$//')
+
+            riscv32-unknown-elf-as -march=$march $arquivo -o build/$nome_sem_extensao.o
+            riscv32-unknown-elf-ld build/$nome_sem_extensao.o -o build/$nome_sem_extensao.elf
+            riscv32-unknown-elf-objcopy -O binary build/$nome_sem_extensao.elf build/$nome_sem_extensao.bin
+            hexdump -v -e '1/4 "%08x" "\n"' build/$nome_sem_extensao.bin > memory/$nome_sem_extensao.hex
+
+            echo "$arquivo compilado"  # Faça o que desejar com o arquivo
+        fi
+    done
+else
+    echo "O diretório não existe."
+fi
