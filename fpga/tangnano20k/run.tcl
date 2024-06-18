36a35d16a7f90158786b272b7f5b22c4cf2b029c
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-06-18 14:00:27 -0300

    Adicionando testes para as instrucões novas
    Adicionando testes para instrucões de multiplicacão e divisão

--- fpga/tangnano20k/run.tcl
+++ fpga/tangnano20k/run.tcl
@@ -12,6 +12,7 @@ add_file ../../src/core/mux.v
 add_file ../../src/core/pc.v
 add_file ../../src/core/registers.v
 add_file ../../src/core/csr_unit.v
+add_file ../../src/core/mdu.v
 add_file ../../src/peripheral/bus.v
 add_file ../../src/peripheral/gpio.v
 add_file ../../src/peripheral/gpios.v
---
+++ software/code/mul.s
@@ -0,0 +1,7 @@
+.globl _start
+
+_start:
+    li a0, 10;
+    li a1, 20;
+
+    mul a2, a0, a1;
