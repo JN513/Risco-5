292c539d43b88ce625e2dbc2b0a38d6c6b26448b
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-07-10 12:46:32 -0300

    Alterando flags de build do iverilog e adicionando arquivo de config na raiz da pasta src

--- run_test.sh
+++ run_test.sh
@@ -7,7 +7,7 @@ read nome_do_teste
 
 cp software/memory/$nome_do_teste.hex software/memory/generic.hex
 
-iverilog -o build/soc_test.o -s soc_tb src/core/*.v src/peripheral/*.v tests/soc_test.v
+iverilog -o build/soc_test.o -s soc_tb -I src/ src/core/*.v src/peripheral/*.v tests/soc_test.v
 vvp build/soc_test.o
 
 rm software/memory/generic.hex
